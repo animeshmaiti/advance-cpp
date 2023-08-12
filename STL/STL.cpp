6c0e2cca8b2ff36e4767de338284ebb4b4a2bd5f
Animesh Maiti <a.maiti770@gmail.com>
2023-08-12 11:01:17 +0530

    STL  vector,auto,push_back

--- STL/STL.cpp
+++ STL/STL.cpp
@@ -13,8 +13,11 @@ int main(){
     double d{Add(1.1,2.2)};
     string s{Add(string{"hello "},string{"world"})};
 
-    // int j{Add(s,2)};
-    vector<int> integer{3,7,11,23};
+    // int j{Add("34",2)};
+    // int j{Add(d,2)};you need to give same data type
+
+    int j{Add(i,2)}; // this is possible
+    vector<int> integer{3,7,11,23};// integer vector will automatically resize your list according to elements
     vector<double> doubles{1.1,5.4,77.2,-1.2};
     int ilength=integer.size();
     int dlength=doubles.size();
@@ -25,6 +28,22 @@ int main(){
     ilength=integer.size();
     integer[0]++;
     auto third=integer[2];
+
+    int total{0};
+    for (int i = 0; i < integer.size(); i++)
+    {
+        total +=integer[i];
+    }
+    
+    int total2{0};
+    /*it will run for all integer value and update elem to according integer values.
+    auto will automaticaly define the data type of elem*/
+    for( const auto& elem : integer ){
+        total2+=elem;
+    }
+
+    // int total3=accumulate(begin(integer),end(integer),0);
+
 
     return 0;
 }
